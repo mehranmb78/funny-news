d0359505f5788b8dbdc9864d97852f1d1054fd3f
Mehran Mohammad Beiglou <mehranmb78@gmail.com>
2023-12-13 07:19:24 +0100

    feat(jest): remove karma and jasmin , then setup jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
