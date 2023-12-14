b5d89f83ac08226ae45f593f531cd63bbb269e84
Mehran Mohammad Beiglou <mehranmb78@gmail.com>
2023-12-14 12:11:00 +0100

    fix(routing): solve routing issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: PostsComponent,
-  },
+  { path: '', component: PostsComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/' },
 ];
